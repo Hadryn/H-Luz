51420bc8ea53eff71681f86251878d52e3ab3ac0
Hadryn <hadryndev@gmail.com>
2021-10-20 12:36:04 -0300

    update 2.0.2

--- src/com/h_dev/luz/Main.java
+++ src/com/h_dev/luz/Main.java
@@ -2,12 +2,17 @@ package com.h_dev.luz;
 
 import com.h_dev.luz.comandos.ComandoLuz;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    public static FileConfiguration config;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        config = getConfig();
         Bukkit.getConsoleSender().sendMessage("§aO PLUGIN FOI INICIADO COM SUCESSO!!");
         Bukkit.getConsoleSender().sendMessage("§aPlugin criado por Hadryn!");
         Bukkit.getConsoleSender().sendMessage("§aSolicite seu plugin no discord Hadryn#8324");
--- src/com/h_dev/luz/comandos/ComandoLuz.java
+++ src/com/h_dev/luz/comandos/ComandoLuz.java
@@ -1,6 +1,9 @@
 package com.h_dev.luz.comandos;
 
+import com.h_dev.luz.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,17 +17,23 @@ public class ComandoLuz implements CommandExecutor {
 
         if(sender instanceof Player){
             Player p = (Player) sender;
+            String activemsg = (Main.config.getString("active-msg").replace("&", "§"));
+            String deactivemsg = (Main.config.getString("deactive-msg").replace("&", "§"));
+            String consoleactivemsg = (Main.config.getString("consoleactive-msg").replace("&", "§"));
+            String consoledeactivemsg = (Main.config.getString("consoledeactive-msg").replace("&", "§"));
 
             if(p.hasPermission("hluz.use")){
 
                 if(!(p.hasPotionEffect(PotionEffectType.NIGHT_VISION))){
                     p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 99999, 1));
-                    Bukkit.getConsoleSender().sendMessage("§aFOI ATIVADA A LUZ PARA "+p.getName());
-                    p.sendMessage("§a A LUZ FOI ATIVADA!");
+                    p.playSound(p.getLocation(), Sound.LEVEL_UP, 500.0f, 1.0f);
+                    Bukkit.getConsoleSender().sendMessage(consoleactivemsg+" "+p.getName());
+                    p.sendMessage(activemsg);
                 }else{
                     p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                    Bukkit.getConsoleSender().sendMessage("§cFOI DESATIVADA A LUZ PARA "+p.getName());
-                    p.sendMessage("§c A LUZ FOI DESATIVADA");
+                    p.playSound(p.getLocation(), Sound.BLAZE_DEATH, 500.0f, 1.0f);
+                    Bukkit.getConsoleSender().sendMessage(consoledeactivemsg+" "+p.getName());
+                    p.sendMessage(deactivemsg);
                 }
             }else{
                 p.sendMessage("§cCOMANDO NEGADO PELO ADMINISTRADOR!");
